731a36b848ba3c673faf1df01856ebc6096543d0
Jan Eubel <eubel.jan@gmail.com>
2025-01-17 12:10:44 +0100

    delete .idea changes

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import subprocess
 import os
-import sys
+import shlex
 
+def split_command(command: str):
+    return shlex.split(command)
 
-def main():
+def run_command(raw_command, path):
     try:
-        # Absoluter Pfad zur render_agent.py
-        script_path = os.path.abspath("./ns_policies/SCoBOts_framework/render_agent.py")
+        script_path = os.path.abspath(path)
         if not os.path.exists(script_path):
             print(f"Die Datei wurde nicht gefunden: {script_path}")
             return
 
-        command = ["python", script_path, "-g", "Pong", "-s", "0", "-r", "human", "-p", "default"]
+        command = split_command(raw_command)
+        command[1] = script_path
 
-        # Subprozess starten
         subprocess.run(command, check=True)
         print("Befehl erfolgreich ausgeführt.")
+
     except subprocess.CalledProcessError as e:
         print(f"Fehler beim Ausführen des Befehls: {e}")
     except FileNotFoundError as e:
@@ -23,5 +25,28 @@ def main():
     except Exception as e:
         print(f"Ein unerwarteter Fehler ist aufgetreten: {e}")
 
+def use_scobots():
+    original_command = "python render_agent.py -g Pong -s 0 -r human -p default"
+    path = "./ns_policies/SCoBOts_framework/render_agent.py"
+    run_command(original_command, path)
+
+def use_blendrl():
+    original_command = "python3 play_gui.py --env-name donkeykong --agent-path ./ns_policies/blendrl/models/donkeykong_demo"
+    path = "./ns_policies/blendrl/play_gui.py"
+    run_command(original_command, path)
+
+def main(policy_type):
+    match policy_type:
+        case "scobots":
+            print("using scobots")
+            use_scobots()
+        case "blendrl":
+            print("using blendrl")
+            use_blendrl()
+        case _:
+            print("unknown policy type")
+
+
 if __name__ == "__main__":
-    main()
+    #main("scobots")
+    main("blendrl")
