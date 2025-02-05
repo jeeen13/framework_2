7849685acccf0db412e0790ff91a432582c828db
NiklasPaprotny <43308182+NiklasPaprotny@users.noreply.github.com>
2025-02-05 21:59:18 +0100

    parser

--- render_agent.py
+++ render_agent.py
@@ -9,30 +9,27 @@ sys.path.append(os.path.join(os.getcwd(), "ns_policies", "blendrl"))
 a = sys.path
 
 from framework_utils.BlendRLRenderer import BlendRLRenderer
-from ns_policies.SCoBOts_framework.utils.parser.parser import render_parser
+from framework_utils.parser import render_parser
 from framework_utils.ScoBotsRenderer import ScoBotsRenderer
 
 
 def main():
-    # agent_name = "scobots"
-    # agent_path = "./ns_policies/SCoBOts_framework/resources/checkpoints/Pong_seed0_reward-human_oc_pruned/best_model.zip"
-    # env_name = "pong"
-    # parser_args = render_parser()
+    parser_args = render_parser()
+    env_name = parser_args["game"]
+    agent_name = parser_args["agent"]
+    fps = parser_args["fps"]
+    render_panes = parser_args["render_panes"]
+    seed = parser_args["seed"]
 
-    agent_name = "blendrl"
-    agent_path = "./ns_policies/blendrl/out/runs/seaquest_softmax_blender_logic_lr_0.00025_llr_0.00025_blr_0.00025_gamma_0.99_bentcoef_0.01_numenvs_5_steps_128_pretrained_False_joint_True_0"
-    env_name = "Seaquest"
-    deterministic = True
-
-    fps= 50
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     screenshot_path = ""
-    render_panes = True
-    seed = 0
 
     if agent_name == "scobots":
+        agent_path = f"./ns_policies/SCoBOts_framework/resources/checkpoints/{parser_args['exp_name']}/best_model.zip"
         renderer = ScoBotsRenderer(agent_path, env_name, fps, device, screenshot_path, render_panes, seed, parser_args)
     elif agent_name == "blendrl":
+        agent_path = "./ns_policies/blendrl/out/runs/seaquest_softmax_blender_logic_lr_0.00025_llr_0.00025_blr_0.00025_gamma_0.99_bentcoef_0.01_numenvs_5_steps_128_pretrained_False_joint_True_0"
+        deterministic = True
         renderer = BlendRLRenderer(agent_path=agent_path,
                                env_name=env_name,
                                fps=fps,
